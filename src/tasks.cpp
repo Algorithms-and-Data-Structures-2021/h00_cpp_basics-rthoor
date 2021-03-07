97a237ec8e5fc7d8a8b18ce698e3f89ce570821b
Arthur <rthoor@mail.ru>
2021-03-07 20:31:59 +0300

    fixed bug

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -56,7 +56,7 @@ bool copy_2d_array(int **arr_2d_source, int **arr_2d_target, int num_rows, int n
 // Задание 4
 void reverse_1d_array(vector<int> &arr) {
     for(int j = 0; j < (arr.size()/2); j++){
-        arr.insert(arr.begin(), arr.size()-j);
+        arr.insert(arr.begin(), arr[arr.size()-j]);
         arr.erase(arr.end()-j);
         arr.insert(arr.end(), arr[j+1]);
         arr.erase(arr.begin()+j+1);
